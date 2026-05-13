8f9a315a89cdd6b420ffbe927c56e654071ed1a8
AgusNorambuena <agustinnoram@gmail.com>
2026-05-13 10:27:42 -0300

    feature: agrego la App-1 como carpeta no como proyecto .git

---
+++ App-1/app/(tabs)/saludo.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { ThemedText } from '@/components/themed-text';
+
+export default function Saludo() {
+return (
+<View>
+<ThemedText>Hola desde otra pantalla </ThemedText>
+</View>
+);
+}
